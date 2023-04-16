d5f7f25add4a17c512dc85b73fc5d841260a1a02
Razee <razee.hj@gmail.com>
2023-04-16 22:46:43 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Razee Hussein-Jamal on 2023-04-16.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
