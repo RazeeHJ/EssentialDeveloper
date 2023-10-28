7cd9a3a47072ba53cea1ccc9ec00b59bf1edeaf5
Razee <razee.hj@gmail.com>
2023-10-28 22:41:15 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Razee Hussein-Jamal on 2023-10-28.
+//
+
+import Foundation
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Razee Hussein-Jamal on 2023-10-28.
+//
+
+import Foundation
