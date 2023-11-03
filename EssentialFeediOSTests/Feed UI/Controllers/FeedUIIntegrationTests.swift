49fbaae1c913e1ccdbcc9bae5976d164604c1871
Razee <razee.hj@gmail.com>
2023-11-03 16:26:03 +0100

    Rename FeedViewControllerTests to FeedUIIntegrationTests since we are testing the composition of multiple UI components in integration

--- EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
@@ -10,23 +10,13 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 
-final class FeedViewControllerTests: XCTestCase {
+final class FeedUIIntegrationTests: XCTestCase {
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
         
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
-    }
-    
-    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
-        }
-        return value
     }
     
     func test_loadFeedActions_requestFeedFromLoader() {
