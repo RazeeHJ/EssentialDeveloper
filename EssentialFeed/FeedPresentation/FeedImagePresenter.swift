6c8f6edd36d60a32c877cf15c6f6e2400be8acb8
Razee <razee.hj@gmail.com>
2023-11-12 22:07:56 +0100

    Move `FeedImagePresenter` and collaborators to production

---
+++ EssentialFeed/FeedPresentation/FeedImagePresenter.swift
@@ -0,0 +1,69 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Razee Hussein-Jamal on 2023-10-28.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
+
+protocol FeedImageView {
+    associatedtype Image
+
+    func display(_ model: FeedImageViewModel<Image>)
+}
+
+final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+
+    internal init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false))
+    }
+
+    private struct InvalidImageDataError: Error {}
+
+    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: image,
+            isLoading: false,
+            shouldRetry: false))
+    }
+
+    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true))
+    }
+}
