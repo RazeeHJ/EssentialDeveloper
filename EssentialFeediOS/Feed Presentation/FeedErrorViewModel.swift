e0bea91c859fe218a9b9aab97544a08c3ddef179
Razee <razee.hj@gmail.com>
2023-11-12 19:44:56 +0100

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Razee Hussein-Jamal on 2023-11-12.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
