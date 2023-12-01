844a29c43583101dc9bc617350ff6f97c82e9b9b
Razee <razee.hj@gmail.com>
2023-12-01 14:50:10 +0100

    Check completionDeletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -129,7 +129,7 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         
         expect(sut, toCompleteWith: .failure(deletionError)) {
             store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
-            store.completeRetrieval(with: deletionError)
+            store.completeDeletion(with: deletionError)
         }
     }
     
