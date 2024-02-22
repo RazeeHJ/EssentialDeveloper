29a79ce0060e144e0a3dcd8802460940910e3cda
Razee <razee.hj@gmail.com>
2024-02-23 00:20:51 +0100

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -10,7 +10,7 @@ import EssentialFeediOS
 @testable import EssentialFeed
 
 class ListSnapshotTests: XCTestCase {
-
+    
     func test_emptyList() {
         let sut = makeSUT()
         
@@ -28,12 +28,11 @@ class ListSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
     }
-
+    
     // MARK: - Helpers
     
     private func makeSUT() -> ListViewController {
         let bundle = Bundle(for: ListViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let controller = ListViewController()
         controller.loadViewIfNeeded()
         controller.tableView.separatorStyle = .none
